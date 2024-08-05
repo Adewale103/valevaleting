1e4382aa78f21369d7c621b89be737f0ee118d9e
Adewale103 <adeyinkawale13@gmail.com>
2024-08-05 17:44:14 +0100

    fix db issues

--- src/main/java/com/project/valevaleting/dto/request/BookingRequest.java
+++ src/main/java/com/project/valevaleting/dto/request/BookingRequest.java
@@ -2,6 +2,7 @@ package com.project.valevaleting.dto.request;
 
 import com.project.valevaleting.enums.CarType;
 import com.project.valevaleting.enums.ServiceType;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,10 +12,13 @@ import lombok.Setter;
 public class BookingRequest {
     private String firstName;
     private String lastName;
+    @NotBlank
     private String email;
     private String phoneNumber;
+    @NotBlank
     private String amount;
     private String additionalInfo;
+    @NotBlank
     private String vehicleRegistrationNumber;
     private ServiceType serviceType;
     private CarType carType;
